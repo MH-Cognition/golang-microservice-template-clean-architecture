556215540039df9806f7aebab35b67e4074318ea
harieesh <harirakha12@gmail.com>
2025-12-11 18:02:11 +0530

    Update dependencies in go.mod and go.sum, upgrade Go version to 1.23.0, and implement PostgreSQL connection in the API main function.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,9 +7,17 @@ import (
 	"github.com/MH-Cognition/golang-microservice-template-clean-architecture/internal/repository/inmemory"
 	"github.com/MH-Cognition/golang-microservice-template-clean-architecture/internal/transport/httptransport"
 	"github.com/MH-Cognition/golang-microservice-template-clean-architecture/internal/usecase"
+	"github.com/MH-Cognition/golang-microservice-template-clean-architecture/internal/repository/postgres"
 )
 
 func main() {
+
+	pool, err := postgres.Connect()
+	if err != nil {
+		log.Fatal("❌ Failed to connect to Postgres:", err)
+	}
+
+	log.Println("✅ Connected to Postgres!",pool)
 
 	userRepo := inmemory.NewInMemoryUserRepository()
 
--- internal/repository/postgres/conn.go
+++ internal/repository/postgres/conn.go
@@ -1,2 +1,18 @@
 package postgres
 // db connection placeholder
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func Connect() (*pgxpool.Pool, error) {
+	dsn := "postgres://app:app@localhost:5432/mydb?sslmode=disable"
+
+	pool, err := pgxpool.New(context.Background(), dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
